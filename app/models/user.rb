deaed20bcffae535da97d95af74dac402ae47023
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2014-09-08 17:26:39 +0900

    ユーザーを管理者と作業者に分ける

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+
+  enum role: {admin: 'admin', worker: 'worker'}
 end
